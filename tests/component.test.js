007d6296f3a385a747306e34d0c6e9e6688e8864
Anton Fisher <a.fschr@gmail.com>
2017-09-07 18:03:03 -0700

    feat: add setProps component test

--- tests/component.test.js
+++ tests/component.test.js
@@ -3,21 +3,40 @@ import {shallow} from 'enzyme';
 import TimeField from '../src/index';
 
 describe('Component', () => {
-  let c;
+  let a;
+  let b;
+  let onChangeA;
+  let onChangeB;
 
   beforeEach(() => {
-    c = shallow(<TimeField value={'00:00'} />);
+    onChangeA = jest.fn();
+    onChangeB = jest.fn();
+    a = shallow(<TimeField value={'00:00'} onChange={onChangeA} />);
+    b = shallow(<TimeField value={'00:00:00'} onChange={onChangeB} showSeconds />);
   });
 
   afterEach(() => {
-    c = null;
+    a = null;
+    b = null;
   });
 
   test('should render input field', () => {
-    expect(c.find('input')).toHaveLength(1);
+    expect(a.find('input')).toHaveLength(1);
+    expect(b.find('input')).toHaveLength(1);
   });
 
   test('should render time value from props', () => {
-    expect(c.node.props.value).toEqual('00:00');
+    expect(a.find('input').node.props.value).toEqual('00:00');
+    expect(b.find('input').node.props.value).toEqual('00:00:00');
+  });
+
+  test('should render reserved props', () => {
+    expect(a.setProps({value: '12:34'}).find('input').node.props.value).toEqual('12:34');
+    expect(b.setProps({value: '12:34:56'}).find('input').node.props.value).toEqual('12:34:56');
+  });
+
+  test('should validate reserved props before render', () => {
+    expect(a.setProps({value: '30:60'}).find('input').node.props.value).toEqual('00:00');
+    expect(b.setProps({value: '30:60:90'}).find('input').node.props.value).toEqual('00:00:00');
   });
 });
